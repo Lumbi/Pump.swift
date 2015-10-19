e3bb6b98a331c454f68f4de2f50ea7be90829ba8
Gabriel Lumbi <gabriel.lumbi@reaktor.co.jp>
2015-10-19 23:15:45 +0900

    Initial Commit

---
+++ Pump/Pump.h
@@ -0,0 +1,19 @@
+//
+//  Pump.h
+//  Pump
+//
+//  Created by Gabriel Lumbi on 10/19/15.
+//  Copyright © 2015 Lumbi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Pump.
+FOUNDATION_EXPORT double PumpVersionNumber;
+
+//! Project version string for Pump.
+FOUNDATION_EXPORT const unsigned char PumpVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pump/PublicHeader.h>
+
+
